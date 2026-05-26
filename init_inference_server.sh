2dac1af803ee06c4edde7cce82f9988c81ffa7c4
ajdsouz <aj.anthonydsouza@gmail.com>
2026-05-26 02:32:53 +0200

    modified: init_inference_server.sh
    script now exports model path variable so subsequent scripts can access it

--- init_inference_server.sh
+++ init_inference_server.sh
@@ -4,7 +4,11 @@ MODEL_PATH=$1
 HOST="127.0.0.1"
 PORT="8000"
 
+# set variable so subsequent scripts can access model path / name
+export $MODEL_PATH
+
 vllm serve "$MODEL_PATH" \
     --enforce-eager \
     --host "$HOST" \
     --port "$PORT"
+
