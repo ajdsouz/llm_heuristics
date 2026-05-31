11f05d247cb3ad2c03a7d1810162ee4622cb01de
ajdsouz <aj.anthonydsouza@gmail.com>
2026-05-31 19:15:59 +0200

    bugfix: removed export
    export overwrites model path, causing an empty string

--- init_inference_server.sh
+++ init_inference_server.sh
@@ -5,7 +5,7 @@ HOST="127.0.0.1"
 PORT="8000"
 
 # set variable so subsequent scripts can access model path / name
-export $MODEL_PATH
+#export $MODEL_PATH
 
 vllm serve "$MODEL_PATH" \
     --enforce-eager \
